fc355784e32ad92ac160c99a4f778e14841324f2
dbaalbaki <80474797+dbaalbaki@users.noreply.github.com>
2021-03-22 15:37:58 -0400

    Lab 3.1

---
+++ Lab3.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to lab 3.1!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
